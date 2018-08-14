c7c07bbe47d8defc1ea398fdc3e195b2b2f2c7f7
MRHE <hetorbite@gmail.com>
2018-08-14 17:09:26 -0500

    Vista de index e implementación de plantilla

--- grails-app/init/prueba2/BootStrap.groovy
+++ grails-app/init/prueba2/BootStrap.groovy
@@ -3,21 +3,28 @@ package prueba2
 class BootStrap {
 
     def init = { servletContext ->
-        def adminRole = new Rol(authority: 'ROLE_ADMIN').save()
+        def RoleAdmin = new Rol(authority: 'ROLE_ADMIN').save()
+        def RoleZona = new Rol(authority: 'ROLE_ZONA').save()
+        def RoleSuper = new Rol(authority: 'ROLE_SUPER').save()
 
-        def testUser = new Usuario(username: 'admin', password: 'admin', nombre: 'Admin', apellidoPaterno: 'admin', apellidoMaterno: 'apellidoPaterno').save()
+
+        def testUser = new Usuario(username: 'admin', password: 'admin', nombre: 'Admin', apellidoPaterno: 'admin', apellidoMaterno: 'admin').save()
+        def testUser2 = new Usuario(username: 'zona', password: 'zona', nombre: 'zona', apellidoPaterno: 'zona', apellidoMaterno: 'zona').save()
+        def testUser3 = new Usuario(username: 'super', password: 'super', nombre: 'super', apellidoPaterno: 'super', apellidoMaterno: 'super').save()
 
 
-        UsuarioRol.create testUser, adminRole
+        UsuarioRol.create testUser, RoleAdmin
+        UsuarioRol.create testUser2, RoleZona
+        UsuarioRol.create testUser3, RoleSuper
 
         UsuarioRol.withSession {
             it.flush()
             it.clear()
         }
 
-        assert Usuario.count() == 1
-        assert Rol.count() == 1
-        assert UsuarioRol.count() == 1
+        assert Usuario.count() == 3
+        assert Rol.count() == 3
+        assert UsuarioRol.count() == 3
     }
     def destroy = {
     }
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -5,7 +5,27 @@
     <title>Welcome to Grails</title>
 </head>
 <body>
-    
+
+<h1>Index</h1>
+
+<sec:ifNotLoggedIn>
+    No estas logeado
+    <li class="dropdown user user-menu">
+        <g:link class="btn text-blue" uri="/login/auth">Iniciar sesión</g:link>
+    </li>
+</sec:ifNotLoggedIn>
+<sec:ifLoggedIn>
+    <sec:access expression="hasRole('ROLE_ADMIN')">
+        <h2>Bienvenido Admin</h2>
+    </sec:access>
+    <sec:access expression="hasRole('ROLE_SUPER')">
+        <h2>Bienvenido SUPERUSUARIO</h2>
+    </sec:access>
+    <sec:access expression="hasRole('ROLE_ZONA')">
+        <h2>Bienvenido ZONA</h2>
+    </sec:access>
+
+</sec:ifLoggedIn>
 
 </body>
 </html>
